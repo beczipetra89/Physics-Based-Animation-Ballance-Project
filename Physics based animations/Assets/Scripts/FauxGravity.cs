ed44022b87ef852ff9a779fa5202b7f74343f88e
Petra Béczi <beczi@student.chalmers.se>
2020-11-13 15:36:04 +0100

    Implemented terrain, texture, and particle system

---
+++ Physics based animations/Assets/Scripts/FauxGravity.cs
@@ -0,0 +1,26 @@
+﻿// Create gravitational pull "Attract()" force for a rigidbody  
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravity : MonoBehaviour
+{
+    public float gravity = -50f; //-10f
+
+    public void Attract(Transform body)
+    {
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;  //always face the body to gravity up
+
+        // Add force to the body's rigidbody to be pulled towards the center of planet 
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        // Rotation: between 2 directions, the gravityUp and the bodyUp also by adding the body's current rotation
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+
+        //Move towards the target rotation smoothly using Slerp and some speed value 
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+    
+}
---
+++ Physics based animations/Assets/Scripts/Hot Area/ManageLava.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageLava : MonoBehaviour
+{
+    public GameObject triggering;
+    void Start()
+    {
+        triggering.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider _col)
+    {
+        if (_col.gameObject.CompareTag("Player"))
+        {
+            triggering.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider _col)
+    {
+        if (_col.gameObject.CompareTag("Player"))
+        {
+            triggering.SetActive(false);
+            //Object.Destroy(gameObject, 5.0f);
+        }        
+    }
+
+   
+}
