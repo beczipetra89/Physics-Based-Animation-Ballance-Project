00ae6ba1021b95023834205934b15f21b7538894
Petra Béczi <beczi@student.chalmers.se>
2020-11-13 13:25:23 +0100

    Camera follows each players after they are switched
    Merged SwitchCharacter with the FollowBall script. And the new ball is positioned at the previously active ball's position.

--- Physics based animations/Assets/Scripts/FollorBall.cs
+++ Physics based animations/Assets/Scripts/FollorBall.cs
@@ -1,20 +1,48 @@
-﻿using System.Collections;
+﻿//SWITCH PLAYERS WITH "ENTER" KEY AND CAMERA FOLLOWS THE PLAYER
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FollorBall : MonoBehaviour
 {
+    [Header("Players List")]
+    public GameObject[] balls = new GameObject[4];
+    int currentActive;
+    private Vector3 savedPosition;  // Ball's position
+    [Header("Camera Offset")]
+    public float xOffset;
+    public float yOffset;
+    public float zOffset; 
+   
+
+    void Start()
+    {
+        currentActive = 0;
+        balls[currentActive].SetActive(true);
+        transform.position = balls[currentActive].transform.position + new Vector3(xOffset, yOffset, zOffset);
+    }
+
+    void Update()
+    {
+        transform.position = balls[currentActive].transform.position + new Vector3(xOffset, yOffset, zOffset);
+        transform.LookAt(balls[currentActive].transform.position);
 
 
-      public GameObject target;
-      public float xOffset, yOffset, zOffset;
+        //Switch players
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            savedPosition = balls[currentActive].transform.position;
+            Vector3 previousCamPos = transform.position;
+           
+            // Deactivate the current ball and set the next one active
+            balls[currentActive].SetActive(false);
+            currentActive++;
+            if (currentActive == 4) currentActive = 0;
+            balls[currentActive].SetActive(true);
 
-      // Update is called once per frame
-      void Update()
-      {
-          transform.position = target.transform.position + new Vector3(xOffset, yOffset, zOffset); // camera's position to the target's position but adding some distance
-          transform.LookAt(target.transform.position);
-      }
-     
-
-} 
+            // Set the current ball's position to the previous ball's position that was just deactivated
+            balls[currentActive].transform.position = savedPosition;
+        }
+    }
+}
