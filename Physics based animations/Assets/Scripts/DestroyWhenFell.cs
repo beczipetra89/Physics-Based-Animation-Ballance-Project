cbca9eea84ae303ada4e04d43737876f74d71e84
Petra Béczi <beczi@student.chalmers.se>
2020-11-17 18:45:50 +0100

    Finished building the world and added an end point

---
+++ Physics based animations/Assets/Scripts/DestroyWhenFell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenFell : MonoBehaviour
+{
+    public GameObject go;
+
+    void Update()
+    {
+        if(transform.position.y < -100)
+        {
+            Destroy(go);
+        }
+
+    }
+}
