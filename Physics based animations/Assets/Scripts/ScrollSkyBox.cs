48a4df3d9352007f2a902876a3b0fb4f77f44530
Petra Béczi <beczi@student.chalmers.se>
2020-11-18 22:05:52 +0100

    Changed to scrolling skybox component

---
+++ Physics based animations/Assets/Scripts/ScrollSkyBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollSkyBox : MonoBehaviour
+{
+    //public float rotateSpeed = 1.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Skybox>().material.SetFloat("_Rotation", Time.time * 1.23f);
+    }
+}
