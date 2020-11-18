6482dc445f29dc8ba2bc0e30505e36a9a74df793
Petra Béczi <beczi@student.chalmers.se>
2020-11-18 20:51:21 +0100

    Implemented scrolling background

---
+++ Physics based animations/Assets/Scripts/ScrollBg.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBg : MonoBehaviour
+{
+
+    public float speed;
+    Renderer _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * speed);
+        _renderer.material.mainTextureOffset = offset;
+    }
+}
